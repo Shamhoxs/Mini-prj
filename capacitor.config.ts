600f2f83a2cd71f3d2bb13b8b4a4bf7c31b87993
Shamhoxs <shamilasiva03@gmail.com>
2025-05-18 09:27:51 +0530

    Resolved merge conflicts in config files

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
+<<<<<<< HEAD
   appName: 'toDoApp',
+=======
+  appName: 'ionic-todo-app',
+>>>>>>> 9063bc0 (Initial commit)
   webDir: 'www'
 };
 
